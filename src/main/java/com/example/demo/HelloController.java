d75f15ccad092a569ff347519e4d4ca33deb74be
hyeri1126 <114209093+hyeri1126@users.noreply.github.com>
2024-10-08 16:41:32 +0900

    HelloController 두번째 변경

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         System.out.println("hello!!");
-        return "안녕, World!";
+        return "안녕, 류혜리!";
     }
 }
